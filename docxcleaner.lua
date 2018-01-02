8533e9eb0f7b927122b9a6bc91ad1699707add41
paragasu <paragasu@gmail.com>
2018-01-03 03:56:04 +0800

    remove unnecessary check

--- docxcleaner.lua
+++ docxcleaner.lua
@@ -9,13 +9,9 @@ local m = {}
 m.__index = m
 
 function m.new(tmp_dir, sock_file_path)
-  local tmp_doc = self.tmp_dir .. '/'.. m.get_filename(docx_file)
-  if m.file_exists(docx_file) and not m.file_exists(tmp_doc) then 
-    ngx.log(ngx.ERR, "Generate a clean docx template" .. docx_file)
-  end
-
   local self = setmetatable({}, m)
-  self.tmp_doc = tmp_doc
+  self.tmp_dir = tmp_dir
+  if not self.tmp_dir then error("tmp_dir is missing " .. i(self.tmp_dir)) end
   return self
 end
 
@@ -23,8 +19,6 @@ end
 -- clean docx xml using libreoffice
 -- /usr/bin/libreoffice --headless --convert-to docx --outdir ~/tmp docx_file
 function m.clean_xml(self, input_docx)
-
-  if not self.tmp_dir then error("tmp_dir is missing " .. i(self.tmp_dir)) end
   if not input_docx then error("Missing input file " .. i(input_docx)) end
 
   local prog = exec.new(sock_file)
